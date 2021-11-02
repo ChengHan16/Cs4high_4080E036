c83873372e6e355ea15a40722ebffe136fea4f01
ChengHan16 <55220866+ChengHan16@users.noreply.github.com>
2021-11-02 10:58:56 +0800

    Add files via upload

---
+++ 嵌入式系統與實習/File/2021-11-02/RightTriangle/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 25, 2016
+ *      Author: Jhou
+ */
+#include <stdio.h>
+#include <stdlib.h>
+extern double area_cal();
+extern double sidelength();
+#include "side.h"
+
+int main(void)
+{
+	//float side_a=25, side_b=100;
+	double area;
+	double side_c;
+	side_c = sidelength((double)side_a, (double) side_b);
+	area = area_cal((double)side_a, (double)side_b);
+	printf("THe length of the third side is %5.2lf\n", side_c);
+	printf("The area of this right triangle is %5.2lf\n", area);
+	printf("\n");
+	printf("4080E036\n");
+	printf("4080E036\n");
+	printf("4080E036\n");
+	return EXIT_SUCCESS;
+}
+
